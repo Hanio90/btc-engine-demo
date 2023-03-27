d9e834fac85698fa6a26eba52ab31e8be1bde2cb
hanio90 <hanio90@gmail.com>
2023-03-27 10:28:24 -0400

    updated var with let

--- src/hooks/transactionUpdates.ts
+++ src/hooks/transactionUpdates.ts
@@ -1,15 +1,15 @@
 export const getTransactionUpdates = (transaction: any) => {
   const fetchTransactionUpdate = async () => {
     try {
-      var ws = new WebSocket(
+      const ws = new WebSocket(
         "wss://socket.blockcypher.com/v1/btc/main?token=04906790b7ae493993f6bb919d5ac01b"
       );
-      var count = 0;
+      let count = 0;
       ws.onmessage = function (event) {
         console.log("The Event", event);
-        var shortHash = transaction.block_hash.substring(0, 6) + "...";
-        var total = transaction.total / 100000000;
-        var addrs = transaction.addresses.join(", ");
+        const shortHash = transaction.block_hash.substring(0, 6) + "...";
+        const total = transaction.total / 100000000;
+        const addrs = transaction.addresses.join(", ");
         $("#browser-websocket").before(
           "<div>Confirmed transaction " +
             shortHash +
